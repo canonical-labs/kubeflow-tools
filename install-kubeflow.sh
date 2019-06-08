63b052604f0f1d76db046fcecd09599b7b25adef
Thomas Henckel <thomas@henckel.dk>
2019-06-08 08:39:53 +0200

    Update from KubeFlow 0.5.0 to KubeFlow 0.5.1

--- install-kubeflow.sh
+++ install-kubeflow.sh
@@ -5,7 +5,7 @@ set -u  # fail on undeclared variables
 
 export KFAPP=${KFAPP:-mykubeflow} # used to store the ksonnet artifacts (your kubeflow stack)
 export KS_VER=${KS_VER:-0.13.1}      # the version of ksonnet to use
-export KUBEFLOW_VERSION=${KUBEFLOW_VERSION:-0.5.0}  # the version of kubeflow to use
+export KUBEFLOW_VERSION=${KUBEFLOW_VERSION:-0.5.1}  # the version of kubeflow to use
 export KUBEFLOW_SRC=${KUBEFLOW_SRC:-"${HOME}/kubeflow/${KUBEFLOW_VERSION}"}
 
 # Grab the directory of the scripts, in case the script is invoked from a different path
